02009bfa5a0f4b054d6d4ee370b38b5a3e969351
이상엽 <infightersy@naver.com>
2024-12-31 16:33:14 +0900

    직접 구현하는 배열 리스트2 - 동적 배열 추가

--- src/collection/array/MyArrayListV2Main.java
+++ src/collection/array/MyArrayListV2Main.java
@@ -1,9 +1,10 @@
 package collection.array;
 
-public class MyArrayListV1Main {
+public class MyArrayListV2Main {
 
     public static void main(String[] args) {
-        MyArrayListV1 list = new MyArrayListV1();
+//        MyArrayListV2 list = new MyArrayListV2();
+        MyArrayListV2 list = new MyArrayListV2(2);
         System.out.println("==데이터 추가==");
         System.out.println(list);
         list.add("a");
